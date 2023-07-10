bf44a4c3a3ba42ba54189a1fed3f21d4c7016336
Florenz <florenzthedev@gmail.com>
2023-07-10 16:00:34 -0700

    Initial graphing capability added

--- benchmark.sh
+++ benchmark.sh
@@ -14,6 +14,24 @@ lscpu >> "$testID/machine.txt"
 lsmem >> "$testID/machine.txt"
 uname -a >> "$testID/machine.txt"
 
+echo "Initializing graph..."
+echo "\documentclass[tikz]{standalone}
+\usepackage{pgfplots}
+\pgfplotsset{compat=1.17, 
+cycle list={
+        {red, mark=*, thick},
+        {blue, mark=*, thick},
+        {green, mark=*, thick},
+        {orange, mark=*, thick},
+        {purple, mark=*, thick},
+        {brown, mark=*, thick},
+        {black, mark=*, thick},}
+        }
+
+\begin{document}
+\begin{tikzpicture}
+\begin{axis}[xmode=log,log basis x={2}, xlabel={Input Dataset Size}, ylabel={Seconds}]" > "$testID/graph.tex"
+
 IFS=',' read -r startPow endPow timeoutTime < $1
 equation=$(sed -n '2p' $1)
 for i in $(seq $startPow $endPow); do
@@ -38,8 +56,18 @@ for dir in */ ; do
   cd ..
 done
 
+echo "\legend{" > "$testID/legend.tex"
 tail -n +3 $1 | while IFS=',' read -r benchType benchExec benchName auxStart auxEnd
 do
+  if [ "$benchType" == "L" ]; then
+    echo "${benchName}," >> "$testID/legend.tex"
+    echo "\addplot table[col sep=comma,header=false,x index=0,y index=1] {${benchName}.txt};" >> "$testID/graph.tex"
+  elif [ "$benchType" == "T" ]; then
+    for j in $(seq $auxStart $auxEnd); do
+      echo "${benchName}_${j}," >> "$testID/legend.tex"
+      echo "\addplot table[col sep=comma,header=false,x index=0,y index=1] {${benchName}_${j}.txt};" >> "$testID/graph.tex"
+    done
+  fi
   for i in $(seq $startPow $endPow); do
     power=$((2**$i))
     if [ "$benchType" == "L" ]; then
@@ -63,3 +91,16 @@ do
     fi
   done
 done
+
+echo "Generating graph..."
+legend=$(<"$testID/legend.tex")
+rm "$testID/legend.tex"
+legend="${legend::-1}}"
+legend="${legend//_/\\_}"
+echo "$legend" >> "$testID/graph.tex"
+echo "\end{axis}
+\end{tikzpicture}
+\end{document}" >> "$testID/graph.tex"
+cd "$testID/"
+pdflatex graph.tex > /dev/null
+cd ..
